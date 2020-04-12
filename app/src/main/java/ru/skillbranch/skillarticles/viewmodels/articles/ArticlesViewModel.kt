2783d104285cd234ad6e04b014408223fcf61695
harrysia11 <56597821+harrysia11@users.noreply.github.com>
2020-04-12 21:48:51 +0300

    ArticlesViewModel

---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/articles/ArticlesViewModel.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.skillarticles.viewmodels.articles
+
+import androidx.lifecycle.SavedStateHandle
+import ru.skillbranch.skillarticles.data.repositories.ArticleRepository
+import ru.skillbranch.skillarticles.viewmodels.ArticleState
+import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
+import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
+
+class ArticlesViewModel(handle: SavedStateHandle): BaseViewModel<ArticleState>(handle,ArticlesState()) {
+    val repository = ArticleRepository
+
+    init {
+        subscribeOnDataSource(repository.loadArticles()){articles, state ->
+            articles ?: return@subscribeOnDataSource null
+            state.copy(articles = articles)
+        }
+    }
+}
+data class ArticlesState(
+    val articles: List<ArticleItemData> = emptyList()
+): IViewModelState
