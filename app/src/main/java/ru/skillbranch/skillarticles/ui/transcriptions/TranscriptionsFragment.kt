e7713cb8eaf813530d8fb4c39cc6b563527b53ce
Harry Sia <harrysia11@gmail.com>
2020-08-04 20:12:08 +0300

    поправил структуру проекта

--- app/src/main/java/ru/skillbranch/skillarticles/ui/transcriptions/TranscriptionsFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/transcriptions/TranscriptionsFragment.kt
@@ -5,17 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.viewModels
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.viewmodels.transcriptions.TranscriptionsViewModel
 
 class TranscriptionsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = TranscriptionsFragment()
-    }
 
-    private lateinit var viewModel: TranscriptionsViewModel
+//    private lateinit var viewModel: TranscriptionsViewModel
+    private val viewModel : TranscriptionsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +24,7 @@ class TranscriptionsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TranscriptionsViewModel::class.java)
+ //       viewModel = ViewModelProviders.of(this).get(TranscriptionsViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/transcriptions/TranscriptionsViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/transcriptions/TranscriptionsViewModel.kt
@@ -1,7 +1,8 @@
 package ru.skillbranch.skillarticles.viewmodels.transcriptions
 
 import androidx.lifecycle.ViewModel
+import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 
-class TranscriptionsViewModel : ViewModel() {
+class TranscriptionsViewModel : ViewModel(), IViewModelState {
     // TODO: Implement the ViewModel
 }
